dc47094767617ba58d60293d4518352be33f9e13
Kevin Ukota <z1ppm4n@gmail.com>
2024-09-07 09:58:30 +0200

    Create function to get data from a location and console log it.

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+const mainDiv = document.querySelector(".main");
 
+async function getWeather(location) {
+  const weather = await fetch(
+    `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=UKW374H6M7NGVTQZVUPYTDVGN
+  `,
+    { mode: `cors` }
+  );
+
+  const weatherData = await weather.json();
+
+  console.log(weatherData);
+}
+
+getWeather("Koper");
